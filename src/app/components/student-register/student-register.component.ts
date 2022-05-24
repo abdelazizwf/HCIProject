b638765e01366375b59e9bab12cc8134979c799a
ahmedtarek <ahmedtarek114986519623@gmail.com>
2022-05-24 14:26:57 +0200

    updated register

--- src/app/components/student-register/student-register.component.ts
+++ src/app/components/student-register/student-register.component.ts
@@ -15,6 +15,10 @@ export class StudentRegisterComponent implements OnInit {
         this.studentRegisterForm = this.formBuilder.group({
             name: ['', [Validators.required, Validators.maxLength(256)]],
             email: ['', [Validators.required, Validators.email]],
+            nationalID:['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+            age:['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(2)]],
+            phoneNumber:['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(11)]],
+            level:['', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(1)]],
         });
     }
 
@@ -25,4 +29,21 @@ export class StudentRegisterComponent implements OnInit {
     get email() {
         return this.studentRegisterForm.get('email');
     }
+
+    get nationalID() {
+        return this.studentRegisterForm.get('nationalID');
+    }
+    
+    get age() {
+        return this.studentRegisterForm.get('age');
+    }
+    
+    get phoneNumber() {
+        return this.studentRegisterForm.get('phoneNumber');
+    }
+    
+    get level() {
+        return this.studentRegisterForm.get('level');
+    }
+
 }
