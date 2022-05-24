c496db06113abe6f6258d9b3f7d8c2cadfbcf25c
Abdelaziz Farahat <abdelaziz.w.f@gmail.com>
2022-05-24 21:56:48 +0200

    got rid of empty css files

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { PendingApprovalsComponent } from './components/pending-approvals/pending-approvals.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PendingApprovalsComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
