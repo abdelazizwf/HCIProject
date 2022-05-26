bf496e1219e2a55eef4b4bbb31fcb76128c0bf8e
ahmedtarek <ahmedtarek114986519623@gmail.com>
2022-05-26 07:40:06 +0200

    updated login and added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentRegisterComponent } from './components/student-register/student-register.component';
 import { FacultyRegisterComponent } from './components/faculty-register/faculty-register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
     {
@@ -16,6 +17,10 @@ const routes: Routes = [
         path: 'admin',
         loadChildren: () =>
             import('./admin/admin.module').then((m) => m.AdminModule),
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     },
 ];
 
